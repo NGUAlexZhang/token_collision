d8d7076f156e1a3d5f49ed9ed5d0263d00ec2d04
ZhangZhe <alexzhangaccn@gmail.com>
2024-10-24 21:06:12 +0800

    add: close sync with stdio

--- src/utils/main.cc
+++ src/utils/main.cc
@@ -4,7 +4,11 @@
 #include <string>
 
 int main(int argc, char** argv){
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
     if(argc < 2){
+        std::cout << "Usage analyzer <filename>";
         return 1;
     }
     Sam sam;
