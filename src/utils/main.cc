7116fd847b4e4cc43b7453b23087220453cd970a
ZhangZhe <alexzhangaccn@gmail.com>
2024-10-24 21:57:14 +0800

    add: opts

--- src/utils/main.cc
+++ src/utils/main.cc
@@ -2,18 +2,34 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <exception>
+#include <algorithm>
 
 int main(int argc, char** argv){
     std::ios_base::sync_with_stdio(0);
-    std::cin.tie(0);
     std::cout.tie(0);
-    if(argc < 2){
-        std::cout << "Usage analyzer <filename>";
+    if(argc < 3){
+        std::cerr << "Usage analyzer <filename> <minimum occurance frequency>";
         return 1;
     }
+    try{
+        auto frequency = std::stoi(argv[2]);
+        if(frequency <= 0){
+            throw std::runtime_error("frequency must large than 0");
+        }
+    }
+    catch(const std::exception& e){
+        std::cerr << e.what() << std::endl;
+    }
+    auto frequency = std::stoi(argv[2]);
     Sam sam;
     std::ifstream ifs(argv[1]);
     std::string str;
+    if(!ifs.is_open()){
+        std::cerr << "Cannot open file: " << argv[1] << std::endl;
+        return 1;
+    }
+    ifs.tie(0);
     while(ifs >> str){
         sam.addSubstrStatistic(str);
         for(auto ch : str){
@@ -21,10 +37,9 @@ int main(int argc, char** argv){
         }
         sam.extend(0);
     }
-    //sam.print();
     auto statistic = sam.getStatistic();
     for(auto p : statistic){
-        if(p.second + 1 >= 3)
+        if(p.second + 1 >= frequency)
             std::cout << p.first << " " << p.second + 1 << std::endl;
     }
     return 0;
